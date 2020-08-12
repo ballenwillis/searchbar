5cf677f4c327887311fe53a54076e484c1921d8c
Brandon Willis <ballenwillis@gmail.com>
2020-08-12 18:36:56 -0500

    Search bar done

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
+  }
+
+  state = {}
+
+  handleSubmit(event) {
+    const { value } = this.input.current
+    console.log(value)
+    // Hit the API with this and display results
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div style={{margin: '20px'}}>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search term: 
+            <input type="text" ref={this.input} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
